7d8e60d7f5308b9369d2ec95e153e5197a2e56ea
Théo <theo@gmail.com>
2025-10-14 16:17:14 +0400

    Modification backend

---
+++ backend/tests/tests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertTrue(true, "Le backend fonctionne correctement !");
+    }
+}
